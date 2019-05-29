37ea6f58c1098a58d9a47ac4e121a2cfca21f25b
Ivo Angelov <jagdcake@tuta.io>
2019-05-29 17:35:35 +0300

    Create option and configure apps to close

--- config/config.go
+++ config/config.go
@@ -1,5 +1,11 @@
 package config
 
+var AppsToClose = map[string]string{
+	"Transmission": "transmission-gtk",
+	"Firefox":      "firefox",
+	"Steam":        "steam",
+}
+
 const (
 	TIME_TO_SHUTDOWN string = "10s"
 	TIME_TO_REBOOT   string = "5s"
