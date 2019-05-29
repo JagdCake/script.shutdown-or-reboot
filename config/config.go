939a69829d9cae6ee0aa6370cca4fcdf733083cc
Ivo Angelov <jagdcake@tuta.io>
2019-05-29 17:25:31 +0300

    Create and configure shutdown and log file options

---
+++ config/config.go
@@ -0,0 +1,7 @@
+package config
+
+const (
+	TIME_TO_SHUTDOWN string = "10s"
+	TIME_TO_REBOOT   string = "5s"
+	LOG_FILE         string = "/home/jagdcake/Documents/text_files/my_logs/shutdown_log"
+)
