bda64aa6f5b20865b2855839114e04f782e1d321
Ivo Angelov <jagdcake@tuta.io>
2019-05-29 18:42:31 +0300

    Create function to handle select menu input

--- helpers/select-menu.go
+++ helpers/select-menu.go
@@ -3,10 +3,21 @@ package helpers
 import (
 	"fmt"
 	"github.com/jagdcake/shutdown-or-reboot/menu"
+	"strconv"
 )
 
 func printMenu(menuOptions []string) {
 	sm := menu.CreateWith(menuOptions)
 
 	fmt.Print(sm)
+}
+
+func userChoice() int {
+	var input string
+	var choice int
+
+	input = menu.Prompt()
+	choice, _ = strconv.Atoi(input)
+
+	return choice
 }
