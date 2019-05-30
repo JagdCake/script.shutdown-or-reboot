ea9de98881211db9d26ddcefe2b787fe06fbd1a3
Ivo Angelov <jagdcake@tuta.io>
2019-05-30 16:40:50 +0300

    Set event and time to shutdown based on select menu choice

--- main.go
+++ main.go
@@ -14,12 +14,28 @@ func main() {
 	}
 	var choice int = helpers.SelectMenu(menuOptions)
 
-	println(choice)
+	var event string
+	var timeToShutdown string
+
+	switch choice {
+	case 1:
+		event = "shutdown"
+		timeToShutdown = config.TIME_TO_SHUTDOWN
+	case 2:
+		event = "reboot"
+		timeToShutdown = config.TIME_TO_REBOOT
+	case 3:
+		return
+	default:
+		fmt.Println("Please enter option number!")
+		main()
+		return
+	}
 
 	var closedApps string = helpers.CloseApps(config.AppsToClose)
 	fmt.Println(closedApps)
 
-	var shutdownLogged bool = helpers.LogShutdown("shutdown", config.TIME_TO_SHUTDOWN)
+	var shutdownLogged bool = helpers.LogShutdown(event, timeToShutdown)
 	if shutdownLogged {
 		fmt.Println("Shutdown has been logged in", config.LOG_FILE)
 	}
